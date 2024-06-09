d609ec5698d6d6c0aaa20b4239f86c7775618149
maris-svirksts <maris.svirksts@gmail.com>
2024-06-09 16:50:28 +0300

    Cleanup

--- AWS/modules/codeartifact/main.tf
+++ AWS/modules/codeartifact/main.tf
@@ -7,7 +7,7 @@ resource "aws_codeartifact_repository" "repository" {
   domain     = aws_codeartifact_domain.domain.domain
 }
 
-data "aws_iam_policy_document" "codeartifact_policy" {
+/*data "aws_iam_policy_document" "codeartifact_policy" {
   statement {
     actions   = ["codeartifact:*"]
     resources = ["*"]
@@ -17,4 +17,4 @@ data "aws_iam_policy_document" "codeartifact_policy" {
 resource "aws_codeartifact_domain_permissions_policy" "policy" {
   domain          = aws_codeartifact_domain.domain.domain
   policy_document = data.aws_iam_policy_document.codeartifact_policy.json
-}
+}*/
